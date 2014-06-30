b8ce7229d50619b68b714d78b0065faad5dcbf01
Steven Batchelor <admin@ovgl.org>
2014-06-30 12:33:16 -0400

    Added "make install" and "make uninstall"

---
+++ ovgl.pc.cmake
@@ -0,0 +1,6 @@
+Name: ovgl
+Description: Open Video Game Library
+Requires: gl, sdl2, assimp, freeimage, freetype, ffmpeg, bullet
+Version: @OVGL_VERSION@
+Libs: -L@LIB_DESTINATION@ -lovgl
+Cflags: -I@INCLUDE_INSTALL_DIR@
